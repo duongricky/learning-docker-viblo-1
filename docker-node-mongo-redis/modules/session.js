c64d35daf3d1735742fdd59137433fd68562dc2d
Mai Trung Duc <maitrungduc1410@gmail.com>
2019-12-22 17:01:46 +0800

    add docker-node-mongo-redis

---
+++ docker-node-mongo-redis/modules/session.js
@@ -0,0 +1,20 @@
+const session = require('express-session')
+const redis = require('redis')
+const RedisStore = require('connect-redis')(session)
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || '127.0.0.1', // this must match the container name of redis image
+  port: process.env.REDIS_PORT || 6379
+})
+
+module.exports = session({
+  store: new RedisStore({
+    client
+  }),
+  secret: 'supersecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production'
+  },
+  proxy: true
+})
